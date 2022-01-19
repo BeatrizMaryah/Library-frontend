f15ab467647113e73b0606b15f18dc566b6327dc
Beatriz Maryah <beatrizmaryah16@gmail.com>
2022-01-19 14:56:06 -0300

    Adicionando services e models

---
+++ src/app/core/model/book.ts
@@ -0,0 +1,8 @@
+export interface Book{
+  id: number;
+  name: string;
+  author: string;
+  cost: number;
+  isBorrowed: boolean;
+  idLibrary: number;
+}
--- src/app/core/services/books/books.service.ts
+++ src/app/core/services/books/books.service.ts
@@ -1,9 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Book } from '../../model/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor() { }
+  private _book = new Subject<Book>();
+
+  private baseUrl = `${environment.baseUrl}/books`
+
+  constructor(private http: HttpClient) {}
+
+  getOne(id: number){
+    return this.http.get<Book>(`${this.baseUrl}/${id}`)
+  }
+
+  all(){
+    return this.http.get<Book[]>(this.baseUrl);
+  }
+
+  upsert(book: Book){
+    book.cost = Number(book.cost);
+    book.idLibrary = Number(book.idLibrary);
+    book.isBorrowed = Boolean(book.isBorrowed);
+
+    if(book.id){
+      return this.http.patch<Book>(`${this.baseUrl}/${book.id}`, book);
+    } else {
+      return this.http.post<Book>(this.baseUrl, book);
+    }
+  }
+
+  delete(id: number){
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  getBook(): Observable<Book> {
+    return this._book.asObservable();
+  }
+
+  setBook(book: Book){
+    this._book.next(book);
+  }
 }
